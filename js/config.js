237bd88e0fa29d747b10af31e91e1b6eb9101313
Danilo Lema <danilolema@Danilos-MacBook-Pro.local>
2026-01-22 11:07:06 +0100

    new file

--- js/config.js
+++ js/config.js
@@ -2,5 +2,6 @@
 // Ejemplo de formato:
 //   https://docs.google.com/spreadsheets/d/TU_ID/export?format=csv
 // Sustituye esta constante por tu URL real:
+//https://docs.google.com/spreadsheets/d/1v_5lW0DTAWPML2ZN2JYibYmA1IvV9eozg7OpcbFuTWo/edit?usp=sharing
 //https://docs.google.com/spreadsheets/d/1hPd-k8t_s1qBc6OSQcChGiS--GzlQjpT/edit?usp=sharing&ouid=103536115419661710134&rtpof=true&sd=true
-const CSV_URL = "https://docs.google.com/spreadsheets/d/1hPd-k8t_s1qBc6OSQcChGiS--GzlQjpT/export?format=csv";
+const CSV_URL = "https://docs.google.com/spreadsheets/d/1v_5lW0DTAWPML2ZN2JYibYmA1IvV9eozg7OpcbFuTWo/export?format=csv";
